ba3bc6a0ae8f9dbbb1d66996b6467489afea2169
404NotFoundv2 <j8tjuh@gmail.com>
2018-11-01 16:33:38 +0100

    VisirInverter

---
+++ PynPoint/ProcessingModules/VisirInverter.py
@@ -0,0 +1,85 @@
+'''
+Module that subtracts the different Nod positions from each other.
+This Module should run *after* the chop subtraction.
+
+It assumes that the number of frames taken in every Nod position is the same.
+
+@Jasper Jonker
+'''
+
+import numpy as np
+import sys
+from PynPoint.Core.Processing import ProcessingModule
+
+
+class VisirInverterModule(ProcessingModule):
+    def __init__(self,
+                 name_in="Inverter",
+                 image_in_tag="image_in",
+                 image_out_tag="image_out"):
+        '''
+        Constructor of the VisirNodSubtractionModule
+        :param name_in: Unique name of the instance
+        :type name-in: str
+        :param image_in_tag: Entry of the database used as input of the module
+        :type image_in_tag: str
+        :param image_out_tag: Entry written as output
+        :type image_out_tag: str
+        :param subtract: Number of images skipped for each cube (by e.g. using
+        removing the first frame of every nod position, then set subtract to 1)
+        :param subtract: int
+
+        :return: None
+        '''
+
+        super(VisirInverterModule, self).__init__(name_in)
+
+        # Ports
+        self.m_image_in_port1 = self.add_input_port(image_in_tag)
+        self.m_image_out_port1 = self.add_output_port(image_out_tag)
+
+    def run(self):
+        sys.stdout.write("Running VISIRInverterModule...")
+        sys.stdout.flush()
+
+        self.m_cubesize = self.m_image_in_port1.get_attribute("NFRAMES")[0]
+
+        # Check that all NFRAMES are the same
+
+        def inverter(signal_in):
+            '''
+            This function finds the number of images taken every Nod position.
+            Since the different Chop positions are already subtracted, we are
+
+            :return: data_output
+            '''
+
+            self.m_no_cube = int(len(signal_in[:, 0, 0]) / self.m_cubesize)
+
+            data_output = np.zeros(signal_in.shape, np.float32)
+
+            for i in range(self.m_no_cube):
+                if i % 2 == 0:
+                    for ii in range(self.m_cubesize):
+                        data_output[ii + i*self.m_cubesize, :, :] = \
+                            signal_in[ii + i*self.m_cubesize, :, :]
+                else:
+                    for ii in range(self.m_cubesize):
+                        data_output[ii + i*self.m_cubesize, :, :] = \
+                           (-1)*signal_in[ii + i*self.m_cubesize, :, :]
+
+            return data_output
+
+        data_output = inverter(signal_in=self.m_image_in_port1.get_all())
+
+        self.m_image_out_port1.set_all(data_output)
+
+        self.m_image_out_port1.copy_attributes_from_input_port(
+            self.m_image_in_port1)
+        self.m_image_out_port1.add_history_information(
+            "VisirInverterModule", "Inverted the second Nod position")
+
+        self.m_image_out_port1.close_port()
+
+        sys.stdout.write("\rRunning VISIRInverterModule... [DONE]\n")
+        sys.stdout.flush()
