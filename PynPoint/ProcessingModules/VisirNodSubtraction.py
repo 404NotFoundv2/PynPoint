4263f5766169fd4aec76e319d99ab8ff1213d2e7
Jasper Jonker <j8tjuh@gmail.com>
2018-11-05 13:34:03 +0100

    Cleaned Posangle

--- PynPoint/ProcessingModules/VisirNodSubtraction.py
+++ PynPoint/ProcessingModules/VisirNodSubtraction.py
@@ -1,13 +1,14 @@
 '''
-Module that 'subtracts' the different Nod positions from each other.
-Before the 'subtraction', the module derotates the images according to their
-POSANG angle.
+Module that 'subtracts' (it actually adds) the different Nod positions
+from each other. Before the 'subtraction', the module derotates the images
+according to their POSANG angle.
 
 This Module should run *after* the chop subtraction and after
 VisirInverterModule. VisirInverter inverts the images such that here they are
 actually added, not subtracted.
 
-It assumes that the number of frames taken in every Nod position is the same.
+It assumes that the number of frames taken in every Nod position is the same
+and that the star is centered.
 
 @Jasper Jonker
 '''
@@ -16,6 +17,7 @@ import numpy as np
 import sys
 from PynPoint.Core.Processing import ProcessingModule
 from scipy.ndimage import rotate
+import warnings
 
 
 class VisirNodSubtractionModule(ProcessingModule):
@@ -31,9 +33,6 @@ class VisirNodSubtractionModule(ProcessingModule):
         :type image_in_tag: str
         :param image_out_tag: Entry written as output
         :type image_out_tag: str
-        :param subtract: Number of images skipped for each cube (by e.g. using
-        removing the first frame of every nod position, then set subtract to 1)
-        :param subtract: int
 
         :return: None
         '''
@@ -52,29 +51,26 @@ class VisirNodSubtractionModule(ProcessingModule):
         self.m_posang_start = \
             self.m_image_in_port1.get_attribute("PARANG_START")
         self.m_posang_end = self.m_image_in_port1.get_attribute("PARANG_END")
-        # Check that all NFRAMES are the same
 
-        # data = self.m_image_in_port1.get_all()
-        # self.shape_d = data.shape
-        # self.m_no_cube = int(len(data[:, 0, 0]) / self.m_cubesize)
+        for i in range(len(self.m_image_in_port1.get_attribute("NFRAMES"))):
+            if self.m_cubesize != \
+                    self.m_image_in_port1.get_attribute("NFRAMES")[i]:
+                warnings.warn("There is a mismatch in the number of NFRAMES "
+                              "in each cube. They should all be equal in "
+                              "size.")
 
         def posangrot(signal_in):
             '''
             This function interpolates the Posang angle for all images.
+            It takes NODB and derotates it to get the same rotation as
+            NODA has.
             '''
-
-            '''
-            print '\n', 'Posang start: ', self.m_posang_start, '\n'
-            print '\n', 'Posang end: ', self.m_posang_end, '\n'
-            print '\n', 'Number of cubes: ', self.m_no_cube, '\n'
-            '''
-
-            # self.m_posang_start[0]
 
             data_out = np.zeros(signal_in.shape)
             im_rot = np.zeros((self.m_cubesize,
                                signal_in.shape[1],
                                signal_in.shape[2]))
+            self.m_posang = np.zeros((self.m_no_cube*self.m_cubesize))
 
             for i in range(0, self.m_no_cube, 2):
                 posang1 = np.linspace(start=0,
@@ -89,9 +85,9 @@ class VisirNodSubtractionModule(ProcessingModule):
                 posang = np.zeros(posang1.shape)
 
                 for ii in range(self.m_cubesize):
-                    posang[ii] = posang2[ii] - posang1[ii]
+                    posang[ii] = posang1[ii] - posang2[ii]
                     cube1 = signal_in[ii + (i+1)*self.m_cubesize, :, :]
-                    # print 'cube1 shape: ', cube1.shape
+
                     im_rot[ii, :, :] = rotate(input=cube1,
                                               angle=posang[ii],
                                               reshape=False)
@@ -101,9 +97,10 @@ class VisirNodSubtractionModule(ProcessingModule):
                     data_out[ii + (i+1)*self.m_cubesize, :, :] = \
                         im_rot[ii, :, :]
 
-            return data_out
+                    self.m_posang[ii + i*self.m_cubesize] = posang1[ii]
+                    self.m_posang[ii + (i+1)*self.m_cubesize] = posang1[ii]
 
-            # Change the attribute Posangangle
+            return data_out
 
         def sepcube(signal_in):
             '''
@@ -114,15 +111,12 @@ class VisirNodSubtractionModule(ProcessingModule):
             Subtraction of the Nod positions is done in the following way:
             PynPoint saves each Nod position in one big 3D array. The first
             index represents the image number. This index goes from 0 to
-            number_of_nod_positions*images_every_nod_position. This function
+            number_of_nod_rotations*images_every_nod_position. This function
             therefore subtracts images by moving the first index by the amount
             of images taken every Nod position.
 
             :return: data_output
             '''
-
-            # self.shape_d = signal_in.shape
-            # self.m_no_cube = int(len(signal_in[:, 0, 0]) / self.m_cubesize)
 
             data_output = np.zeros((self.m_cubesize*self.m_no_cube/2,
                                     self.shape_d[1], self.shape_d[2]),
@@ -150,7 +144,9 @@ class VisirNodSubtractionModule(ProcessingModule):
         self.m_image_out_port1.copy_attributes_from_input_port(
             self.m_image_in_port1)
         self.m_image_out_port1.add_history_information(
-            "VisirNodSubtractionModule", "Subtracted Nod positions")
+            "VisirNodSubtractionModule", "Subtracted Nod")
+        self.m_image_out_port1.add_attribute("PARANG", self.m_posang,
+                                             static=False)
 
         self.m_image_out_port1.close_port()
 
